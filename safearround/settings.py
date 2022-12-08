2b926e34ada0948af6bc6d70a99dfb608160d63b
gykephra <yaoephra90@gmail.com>
2022-12-08 13:14:25 +0000

    modification des fichiers settings

--- safearround/settings.py
+++ safearround/settings.py
@@ -12,25 +12,22 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os 
-import environ
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env()
-environ.Env.read_env(env_file=str(BASE_DIR / "safearround" / ".env"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG")
+DEBUG = True
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 
@@ -81,12 +78,6 @@ WSGI_APPLICATION = 'safearround.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -124,10 +115,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'mediafiles'
+
+try:
+    from .local_settings import *
+except ImportError:
+    raise('A local settings.py file is required in order to run this application')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
