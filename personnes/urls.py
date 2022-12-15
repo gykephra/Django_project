85ff914133cdf67907eb056ac6d960a93ed64616
gykephra <yaoephra90@gmail.com>
2022-12-15 16:27:00 +0000

    annonce

--- personnes/urls.py
+++ personnes/urls.py
@@ -4,6 +4,7 @@ from . import views
 # one urlpattern per line
 urlpatterns = [
     path('', views.index, name='index'),
+    path('afficher/index/', views.index, name='index'),
     path('formulaire/', views.perso, name ='perso'),
     path('afficher/', views.afficher, name ='afficher'),
 ]
